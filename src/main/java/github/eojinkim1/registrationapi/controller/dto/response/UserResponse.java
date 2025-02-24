4e98465c7c3a6da383f754cdad9c5c1afce781b7
eojinkim1 <rladjwls999@gmail.com>
2025-02-24 17:51:18 +0900

    DTO 추가 - 회원가입 응답 데이터 정의

--- src/main/java/github/eojinkim1/registrationapi/controller/dto/response/UserResponse.java
+++ src/main/java/github/eojinkim1/registrationapi/controller/dto/response/UserResponse.java
@@ -1,4 +1,10 @@
 package github.eojinkim1.registrationapi.controller.dto.response;
 
-public record UserResponse() {
+public record UserResponse(
+        String email,
+        String token,
+        String username,
+        String bio,
+        String image
+){
 }
