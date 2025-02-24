f34957ebb85d54aabcc0e83326e8ac76501768eb
eojinkim1 <rladjwls999@gmail.com>
2025-02-24 17:47:55 +0900

    UserRepository 추가 - 이메일 및 유저네임으로 사용자 조회 기능 구현

---
+++ src/main/java/github/eojinkim1/registrationapi/domain/UserRepository.java
@@ -0,0 +1,53 @@
+package github.eojinkim1.registrationapi.domain;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username); // null이 올 수 있는 값을 감싸는 Wrapper 클래스(Optional<T>)
+    Optional<User> findByEmail(String email);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+*
+* package github.eojinkim1.registrationapi.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
+}
+*/
