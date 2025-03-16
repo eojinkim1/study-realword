a0f671e8c38d1d7a5d4770b3a42880278678d0de
eojinkim1 <rladjwls999@gmail.com>
2025-03-16 19:23:03 +0900

    getCurrentUser 기능 구현

--- src/main/java/github/eojinkim1/registrationapi/service/UserService.java
+++ src/main/java/github/eojinkim1/registrationapi/service/UserService.java
@@ -7,7 +7,6 @@ import github.eojinkim1.registrationapi.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -41,6 +40,21 @@ public class UserService {
                 token,
                 user.getUsername(),
                 user.getBio(),
+                user.getImage()
+        );
+    }
+
+    public UserResponse getCurrentUser(String email, String token) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+
+        String userBio = user.getBio() != null ? user.getBio() : "I work at statefarm";
+
+        return new UserResponse(
+                user.getEmail(),
+                token,
+                user.getUsername(),
+                userBio,
                 user.getImage()
         );
     }
