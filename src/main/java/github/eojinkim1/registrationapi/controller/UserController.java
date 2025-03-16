d9d3982b74d04f8382376a137b0bd2af6dd177ee
eojinkim1 <rladjwls999@gmail.com>
2025-03-16 19:22:14 +0900

    GET /api/user 추가

--- src/main/java/github/eojinkim1/registrationapi/controller/UserController.java
+++ src/main/java/github/eojinkim1/registrationapi/controller/UserController.java
@@ -6,16 +6,17 @@ import github.eojinkim1.registrationapi.controller.dto.request.UserWrapper;
 import github.eojinkim1.registrationapi.controller.dto.response.LoginResponse;
 import github.eojinkim1.registrationapi.controller.dto.response.UserResponse;
 import github.eojinkim1.registrationapi.domain.User;
+import github.eojinkim1.registrationapi.security.JwtUtil;
 import github.eojinkim1.registrationapi.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final JwtUtil jwtUtil;
 
     @PostMapping("/api/users")
     public UserResponse registration(@RequestBody UserWrapper<UserRequest> request) {
@@ -44,5 +45,18 @@ public class UserController {
 
 
         return new LoginResponse(userResponse);
+    }
+
+    @GetMapping("/api/user")
+    public ResponseEntity<LoginResponse> getCurrentUser(@RequestHeader("Authorization")String authHeader) {
+        if(authHeader == null || !authHeader.startsWith("Token ")) {
+            throw new RuntimeException("유효한 토큰이 필요합니다.");
+        }
+        String token = authHeader.substring(6); // 토큰 제거
+        String email = jwtUtil.validateToken(token);
+
+        UserResponse userResponse = userService.getCurrentUser(email, token);
+
+        return ResponseEntity.ok(new LoginResponse(userResponse));
     }
 }
