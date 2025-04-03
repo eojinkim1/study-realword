14c3037181614be5ef3524bd52df50d028567d95
eojinkim1 <rladjwls999@gmail.com>
2025-04-03 21:07:10 +0900

    태그 엔티티 정의 및 이름 기반 태그 관리 구조 추가

---
+++ src/main/java/github/eojinkim1/registrationapi/domain/Tag.java
@@ -0,0 +1,18 @@
+package github.eojinkim1.registrationapi.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+}
