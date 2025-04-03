2826f10f7bb02940fa92ba5f488cde7af33a8843
eojinkim1 <rladjwls999@gmail.com>
2025-04-03 21:02:19 +0900

    slug 기반 게시글 조회 및 Specification 조건 검색 추가

---
+++ src/main/java/github/eojinkim1/registrationapi/repository/ArticleRepository.java
@@ -0,0 +1,11 @@
+package github.eojinkim1.registrationapi.repository;
+
+import github.eojinkim1.registrationapi.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.Optional;
+
+public interface ArticleRepository extends JpaRepository<Article, Long>, JpaSpecificationExecutor<Article> {
+    Optional<Article> findBySlug(String slug);
+}
