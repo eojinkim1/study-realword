e8c8be2e6e78e29776f32c26001906cb5dc007c9
eojinkim1 <rladjwls999@gmail.com>
2025-04-17 18:10:19 +0900

    FollowRepository에 팔로우한 사용자 목록 조회 메서드 추가

--- src/main/java/github/eojinkim1/registrationapi/repository/FollowRepository.java
+++ src/main/java/github/eojinkim1/registrationapi/repository/FollowRepository.java
@@ -3,9 +3,15 @@ package github.eojinkim1.registrationapi.repository;
 import github.eojinkim1.registrationapi.domain.Follow;
 import github.eojinkim1.registrationapi.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
     boolean existsByFollowerAndFollowing(User follower, User following);
     void deleteByFollowerAndFollowing(User follower, User following);
 
+    @Query("SELECT f.following FROM Follow f WHERE f.follower = :viewer")
+    List<User> findFollowedUsers(@Param("viewer") User viewer);
 }
