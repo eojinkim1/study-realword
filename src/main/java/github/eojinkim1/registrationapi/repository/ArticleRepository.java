d6e0340f1b84ada6635e9be94ddfe78deb60846d
eojinkim1 <rladjwls999@gmail.com>
2025-04-17 18:10:41 +0900

    ArticleRepository에 author 리스트 기반 게시글 조회 메서드 추가

--- src/main/java/github/eojinkim1/registrationapi/repository/ArticleRepository.java
+++ src/main/java/github/eojinkim1/registrationapi/repository/ArticleRepository.java
@@ -1,12 +1,18 @@
 package github.eojinkim1.registrationapi.repository;
 
 import github.eojinkim1.registrationapi.domain.Article;
+import github.eojinkim1.registrationapi.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ArticleRepository extends JpaRepository<Article, Long>, JpaSpecificationExecutor<Article> {
     Optional<Article> findBySlug(String slug);
     int countBySlug(String slug);
+
+    Page<Article> findByAuthorIn(List<User> authors, Pageable pageable);
 }
