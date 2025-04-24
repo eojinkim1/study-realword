e3995c5344106152e5c38a2535672070be46ed51
eojinkim1 <rladjwls999@gmail.com>
2025-04-24 22:38:20 +0900

    CommentRepository에 댓글 조회 메서드 추가

--- src/main/java/github/eojinkim1/registrationapi/repository/CommentRepository.java
+++ src/main/java/github/eojinkim1/registrationapi/repository/CommentRepository.java
@@ -1,7 +1,12 @@
 package github.eojinkim1.registrationapi.repository;
 
+import github.eojinkim1.registrationapi.domain.Article;
 import github.eojinkim1.registrationapi.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    // 특정 게시글에 달린 댓글을 시간 순으로 조회
+    List<Comment> findByArticleOrderByCreatedAtAsc(Article article);
 }
